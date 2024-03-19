1dc930068c85ab0e64fa110d3e6d7db84508ab40
Nicolas <nicolasgfalabella@gmail.com>
2024-03-19 08:28:54 -0300

    Cafe Card

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import { fetchCars } from "@/utils";
 export default async function Home() {
 
   const allCars = await fetchCars();
-  console.log(allCars.)
+  console.log(allCars.results)
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
 
   return (
     <main className="overflow-hidden">
@@ -23,8 +24,24 @@ export default async function Home() {
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
           </div>
-
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car)=> (
+                <CafeCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">
+              Opps no se ha encontrado nada
+            </h2>
+            <p> {allCars?.message}</p>
+          </div>
+        )}
 
       </div>
     </main>
