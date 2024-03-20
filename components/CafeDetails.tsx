1cec8806183da1ec80b7a84059edb7c6da0392f2
Nicolas <nicolasgfalabella@gmail.com>
2024-03-20 10:16:47 -0300

    Avance sobre el modal

--- components/CafeDetails.tsx
+++ components/CafeDetails.tsx
@@ -40,8 +40,10 @@ const CafeDetails = ({ isOpen, closeModal, cafe }: CafeDetailtProps) => {
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95">
 
-                                <Dialog.Panel>
+                                <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+
                                     <button type='button'
+                                        className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
                                         onClick={closeModal}>
                                         <Image src="/close.svg"
                                             alt="close"
@@ -49,6 +51,34 @@ const CafeDetails = ({ isOpen, closeModal, cafe }: CafeDetailtProps) => {
                                             height={20}
                                             className='object-contain' />
                                     </button>
+
+                                    <div className='flex-1 flex flex-col gap-3'>
+                                        <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
+
+                                            <Image src="/hero.png"
+                                                fill priority alt="cafe model" className='object-contain' />
+
+                                        </div>
+
+                                        <div className='flex gap-3'>
+                                            <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                                                <Image src="/hero.png" alt="cafe model" fill priority className='object-contain' />
+                                            </div>
+
+                                            <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                                                <Image src="/hero.png" alt="cafe model" fill priority className='object-contain' />
+                                            </div>
+
+                                            <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
+                                                <Image src="/hero.png" alt="cafe model" fill priority className='object-contain' />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className='flex-1 flex flex-col gap-2'>
+                                        <h2 className='font-semibold text-xl capitalize'
+                                        > {cafe.nombre} </h2>
+                                    </div>
                                 </Dialog.Panel>
                             </Transition.Child>
 
